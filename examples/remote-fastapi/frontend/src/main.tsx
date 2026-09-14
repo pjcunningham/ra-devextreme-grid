da1ddc2d8e4e1f2b6733b539a757d5ad1e19ec6d
Paul Cunningham <pjcunningham@borsuk.co.uk>
2026-09-14 11:24:33 +0100

    Configure DX licence

--- examples/remote-fastapi/frontend/src/main.tsx
+++ examples/remote-fastapi/frontend/src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'devextreme/dist/css/dx.light.css';
+import { licenseKey } from './devextreme-license';
+import config from 'devextreme/core/config';
 import './demo.css';
 import { App } from './App';
 
+config({ licenseKey });
 const rootElement = document.getElementById('root');
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
